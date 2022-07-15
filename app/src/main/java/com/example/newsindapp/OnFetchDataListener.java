6736529c90442426047310b96bea21e225e739cd
tauhid <tauhid7870@gmail.com>
2022-07-15 11:47:48 +0530

    First commit

---
+++ app/src/main/java/com/example/newsindapp/OnFetchDataListener.java
@@ -0,0 +1,12 @@
+package com.example.newsindapp;
+
+import com.example.newsindapp.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+
+
+}
